142eed1c65640d5b1ab7a926c775536309d3dfb6
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2020-01-26 01:02:09 +0200

    lesson8 hw task2 in progress

---
+++ Pirkiniai/Pirkiniai/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pirkiniai;
+
+namespace Pirkiniai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pirkinys> PirkiniuSarasas = new List<Pirkinys>();
+            PirkiniuSarasas.Add(new Pirkinys("Alus", 24, 0.50m ))
+        }
+    }
+}
